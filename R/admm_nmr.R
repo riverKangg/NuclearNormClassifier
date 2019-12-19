212618ab501bef9c11011e207a59a0403dde9432
Jacqueline Kang <riverkangg@gmail.com>
2019-12-19 16:55:32 +0900

    commit

---
+++ R/admm_nmr.R
@@ -0,0 +1,121 @@
+coef_img <- function(A,x) {
+  p<-dim(A)[1]; q<-dim(A)[2]; n<-dim(A)[3];
+  A_x <- matrix(rep(0, p*q), dim=c(p,q))
+  for (i in 1:n){
+    A_x <- A_x + x[i]*A[,,i]
+  }
+  return(A_x)
+}
+
+# vec 모듈 생략, as.vector 사용
+
+svt <- function(mu,A_x,B,Z) {
+  svt_in <- A_x-B+1/mu*Z
+  result_svd <- svd(svt_in)
+  # thresholding
+  d <- pmax(result_svd$d-1/mu, 0)
+  # return
+  svt_out <- result_svd$u %*% d %*% result_svd$v
+  return(svt_out)
+}
+
+###############################################################
+############################ ADMM #############################
+###############################################################
+
+admm_nmr_fit <- function(max_iter,ep_abs,ep_rel, A,B){
+  p<-dim(A)[1]; q<-dim(A)[2]; n<-dim(A)[3];
+  # step 1
+  h <- array(rep(0,p*q),dim=c(p*q,n))
+  for (n_iter in 1:n){
+    h[,n_iter] <- as.vector(A[,,n_iter])
+    M <- solve(t(H)%*%H+lambd/mu)%*%t(H)
+  }
+
+  # step 2
+  Y<- -B; Z<-0; k<-0; x<-rep(0,n);
+
+  # step 3~5
+  for (i in 1:max_iter){
+    # updating x
+    g_in <- B+Y-1/mu*Z
+    g <- as.vector(g_in)
+    x_new <- M%*%g
+
+    # updating Y
+    A_x_new <- coef_img(A,x_new)
+    Y_new <- svt(mu, A_x_new,B,Z)
+
+    # updating Z
+    Z_new <- Z+mu*(A_x_new-Y_new-B)
+
+    # termination
+    r_pri <- A_x_new-Y_new-B
+    ep_pri <- ep_abs*sqrt(p*q)+ep_rel
+    s_dual <- 1/mu * h %*% as.vector(Y-Y_new)
+    ep_dual <- ep_abs*sqrt(n) + ep_rel
+  }
+
+  x<-x_new; Y<-Y_new; Z<-Z_new; A_x<-A_x_new;
+
+  return(X_new)
+}
+
+########################## clf ##########################
+
+admm_nmr_clf <- function(A,B,label){
+  x <- ADMM_NMR_fit(A,B)
+  A_x <- coef_img(A,x)
+
+  # A_x_i 구하기 + error 구하기
+  label_unique <- unique(label)
+  error_lst <- c()
+  for (i in target_unique){
+    label_tf <- (label==label_unique)
+    x_i <- x[label_tf]
+    A_i <- A[,,label_tf]
+    A_x_i <- coef_img(A_i,x_i)
+
+    diff <- A_x-A_x_i
+    error <- nuclear(diff)
+    error_lst <- append(error_lst,error)
+  }
+  # min error
+  error_tf <- error==min(error)
+  val <- label_unique[error_tf]
+
+  # return
+  return(val)
+}
+
+
+####################### clf top #######################
+
+admm_nmr_clf_top <- function(A,B,label,top_num){
+  x <- ADMM_NMR_fit(A,B)
+  A_x <- coef_img(A,x)
+  # A_x_i 구하기 + error 구하기
+  label_unique <- unique(label)
+  error_lst <- c()
+  for (i in target_unique){
+    label_tf <- (label==label_unique)
+    x_i <- x[label_tf]
+    A_i <- A[,,label_tf]
+    A_x_i <- coef_img(A_i,x_i)
+
+    diff <- A_x-A_x_i
+    error <- nuclear(diff)
+    error_lst <- append(error_lst,error)
+  }
+
+  # top_num
+  error_top <- sort(error_lst)[1:top_num]
+  error_top_tf <- is.element(error_lst,error_top)
+
+  # min error
+  error_min_tf <- (error_lst==min(error))
+  val_min <- label[error_top_tf]
+
+  # return
+  return(val_min[0],val_top)
+}
